09c1ef823c83e0bccebc1c493e55dcfa41bdf080
rosekamau <kamaurose236@gmail.com>
2024-08-09 17:03:17 +0300

    react-hooks-react-router-code-along-v6

---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+
+import UserProfile from "./pages/UserProfile";
+import ErrorPage from "./pages/ErrorPage";
+import routes from "./routes.js";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }, 
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/profile",
+    element: <UserProfile />
+  }
+]);
